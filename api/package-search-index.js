aea9439107d9e8fdca667ff5de018aec22e2a31f
Connor Van Elswyk <ConnorVanElswyk@gmail.com>
2019-05-22 19:08:11 -0400

    api commit
    Signed-off-by: Connor Van Elswyk <ConnorVanElswyk@gmail.com>

---
+++ api/package-search-index.js
@@ -0,0 +1,4 @@
+packageSearchIndex = [{
+    "l": "All Packages",
+    "url": "allpackages-index.html"
+}, {"l": "lambda.client"}, {"l": "lambda.cognito"}, {"l": "lambda.context"}, {"l": "lambda.logger"}];
---
+++ api/type-search-index.js
@@ -0,0 +1,7 @@
+typeSearchIndex = [{"l": "All Classes", "url": "allclasses-index.html"}, {
+    "p": "lambda.client",
+    "l": "MockClient"
+}, {"p": "lambda.client", "l": "MockClientContext"}, {
+    "p": "lambda.cognito",
+    "l": "MockCognitoIdentity"
+}, {"p": "lambda.context", "l": "MockContext"}, {"p": "lambda.logger", "l": "MockLambdaLogger"}];
